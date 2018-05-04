c9770a52fd6a70a38955d13eea8ac7209c4b9d1b
yusufd <cengyusuf@gmail.com>
2018-05-04 11:44:57 +0300

    js dosyalar local e alındı zap proxy uyardı

--- newReadCars.php
+++ newReadCars.php
@@ -17,8 +17,8 @@ include "config.php";
 
     <title>Araç Kiralama Otomasyonu</title>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>  
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap3-typeahead.min.js"></script>  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
  
  </head>
