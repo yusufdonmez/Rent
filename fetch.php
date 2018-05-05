94775a8f27699f81f032a39e16df7cb638c222dc
yusufd <cengyusuf@gmail.com>
2018-05-05 17:18:47 +0300

    fetch.php search car calısıyor. sql injection sorunu devam ediyor query parametresi sıkıntı

--- fetch.php
+++ fetch.php
@@ -2,17 +2,25 @@
 include "checkin.php";
 include "config.php";
 //require('./readDataConfig.php');
-echo "fetch ",$_POST["query"];
+
 if(isset($_POST["query"]))
 {
-  echo $_POST["query"];
- $request = filter_var(mysqli_real_escape_string($link, $_POST["query"]), FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_HIGH);
-echo $request;
+
+ //$request = filter_var(mysqli_real_escape_string($link, $_POST["query"]), FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_HIGH);
+ $request = mysqli_real_escape_string($link, $_POST["query"]);
+ $query = "
+  SELECT * FROM cars 
+  WHERE carName LIKE '%".$request."%' 
+  OR plate LIKE '%".$request."%' 
+ ";
+  $result = mysqli_query($link, $query);
+ /*
+ $result = mysqli_query($link, $query);
   $stmt = mysqli_prepare($link, "SELECT * FROM cars WHERE carName LIKE CONCAT ('%', ?, '%') OR plate LIKE CONCAT ('%', ?, '%')");
   mysqli_stmt_bind_param($stmt, "ss", $request,$request);
   mysqli_stmt_execute($stmt);
   mysqli_stmt_bind_result($stmt, $id, $plate, $gear,$fuel,$carName,$status);
-
+*/
  $data =array();
  $html = '';
  $html .= '
@@ -31,31 +39,45 @@ if($_SESSION["type"] != "musteri"){
   $html .= '</tr>';
 }
 
- //if(mysqli_num_rows($result) > 0)
-if(mysqli_stmt_fetch($stmt))
+ if(mysqli_num_rows($result) > 0)
+//if(mysqli_stmt_fetch($stmt))
  {
-  //while($row = mysqli_fetch_array($result))
-  while (mysqli_stmt_fetch($stmt))
+  while($row = mysqli_fetch_array($result))
+  //while (mysqli_stmt_fetch($stmt))
   {
-   $data[] = $id;//$id"];
+       $data[] = $row["id"];
+   $data[] = $row["plate"];
+   $data[] = $row["gear"];
+   $data[] = $row["fuel"];
+   $data[] = $row["carName"];
+   $data[] = $row["status"];
+   /*$data[] = $id;//$id"];
    $data[] = $plate; //$plate"];
    $data[] = $gear;  //$gear"];
    $data[] = $fuel;  //$fuel"];
    $data[] = $carName; //filter_var($carName"], FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_HIGH);
-   $data[] = $status;  //$status"];
-   $html .= '
+   $data[] = $status;  //$status"];*/
+   /*$html .= '
    <tr>
     <td>'.$id.'</td>   
     <td>'.$plate.'</td>
     <td>'.$gear.'</td>
     <td>'.$fuel.'</td>
     <td>'.$carName.'</td>
-    <td>'.$status.'</td>';
+    <td>'.$status.'</td>';*/
+    $html .='
+    <tr>
+        <td>'.$row["id"].'</td>
+    <td>'.$row["plate"].'</td>
+    <td>'.$row["gear"].'</td>
+    <td>'.$row["fuel"].'</td>
+    <td>'.$row["carName"].'</td>
+    <td>'.$row["status"].'</td>';
 
     if($_SESSION["type"] != "musteri"){
       $html .= '<td>
-    <a href="editCar.php?id='. $id .'" title="Düzenle" data-toggle="modal" data-target="#editModal" data-whatever="'.$id.'" class="btn btn-primary btn-sm">düzenle</a>
-    <a href="deleteCar.php?id='. $id .'" title="Kayıt Sil" data-toggle="tooltip" class="btn btn-danger btn-sm"> sil</a>
+    <a href="editCar.php?id='. $row["id"] .'" title="Düzenle" data-toggle="modal" data-target="#editModal" data-whatever="'.$row["id"].'" class="btn btn-primary btn-sm">düzenle</a>
+    <a href="deleteCar.php?id='. $row["id"] .'" title="Kayıt Sil" data-toggle="tooltip" class="btn btn-danger btn-sm"> sil</a>
     </td>';
     } else{
       $html .= '</tr>';
